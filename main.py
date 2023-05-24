27559934987fe1a1382eef8c5b95e8722ed11b24
Guilherme Pacheco Aparicio <guiaparicio@gmail.com>
2023-05-24 09:05:22 -0300

    Inclusão de contador de erros

--- main.py
+++ main.py
@@ -2,21 +2,29 @@ import json
 import os
 
 def main():
-
     filename = input("Please provide the name of the JSON file to be validated: ")
     process_json_file(filename)
 
-def check_record_length(record):
+def check_record_length(record, i):
 
     limit_size = 32767
+    error_found = False
 
     for key, value in record.items():
         if isinstance(value, str) and len(value) > limit_size:
-            print(f"\nRecord with more than {limit_size} characters found in key '{key}'\n")
+            error_found = True
+            print(f"\nRecord {i} with more than {limit_size} characters found in key '{key}'\n")
             print("-----------------------")
-            exit()
+            # exit()
+        else:
+            print(f"Register: {i} validate!")
+
+    return error_found
+            
 
 def process_json_file(filename):
+
+    errors = 0
 
     if os.path.isfile(filename):
 
@@ -25,8 +33,17 @@ def process_json_file(filename):
 
         data = json.loads(content)
 
+        i = 1
         for record in data.values():
-             check_record_length(record)
+             
+             error_found = check_record_length(record, i)
+
+             if(error_found == True):
+                 errors = errors + 1
+
+             i = i+1
+
+        print(f"Total errors: {errors}")
 
     else:
         print("File not found.")
